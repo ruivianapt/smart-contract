9d33733586bb711c0465b8510016f25a4cddee62
Rui Viana <70287846+ruivianapt@users.noreply.github.com>
2024-05-09 17:10:17 +0100

    sadsadas
    1asdsadsadasdsad

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -21,14 +21,16 @@ contract RingoToken is ERC20, Ownable, ReentrancyGuard, AccessControl {
         mapping(address => bool) hasVoted;
     }
 
-    uint256 public minQuorumPercent = 25; // Minimum quorum needed for proposal votes
     Proposal[] public proposals;
 
     event FeesRedirected(uint256 amount, address to);
     event ProposalCreated(uint256 id, string description);
     event Voted(uint256 id, address voter, bool vote, uint256 weight);
     event ProposalExecuted(uint256 id, bool successful, string description);
-    event RoleUpdated(bytes32 role, address account, bool isGranted);
+    event TransactionFeeAdjusted(uint256 newFeeRate);
+    event TreasuryAddressUpdated(address newAddress);
+    event RoleGranted(bytes32 role, address account);
+    event RoleRevoked(bytes32 role, address account);
 
     constructor(address _treasuryAddress) ERC20("RingoToken", "RNG") {
         _mint(msg.sender, initialSupply);
@@ -57,55 +59,27 @@ contract RingoToken is ERC20, Ownable, ReentrancyGuard, AccessControl {
         return (amount * transactionFeeRate) / 10000;
     }
 
-    function setFeeExemption(address account, bool isExempt) public onlyRole(ADMIN_ROLE) {
-        feeExempted[account] = isExempt;
-        emit RoleUpdated(ADMIN_ROLE, account, isExempt);
-    }
-
-    function createProposal(string memory description) public {
-        require(bytes(description).length > 0, "Description cannot be empty");
-        proposals.push(Proposal({
-            description: description,
-            executed: false,
-            affirmativeVotes: 0,
-            negativeVotes: 0
-        }));
-        emit ProposalCreated(proposals.length - 1, description);
-    }
-
-    function voteOnProposal(uint256 proposalId, bool approve) public {
-        require(proposalId < proposals.length, "Invalid proposal ID");
-        Proposal storage proposal = proposals[proposalId];
-        require(!proposal.hasVoted[_msgSender()], "Already voted");
-        require(!proposal.executed, "Proposal already executed");
-
-        uint256 weight = balanceOf(_msgSender());
-        proposal.hasVoted[_msgSender()] = true;
-
-        if (approve) {
-            proposal.affirmativeVotes += weight;
-        } else {
-            proposal.negativeVotes += weight;
-        }
-        emit Voted(proposalId, _msgSender(), approve, weight);
-    }
-
-    function executeProposal(uint256 proposalId) public {
-        require(proposalId < proposals.length, "Invalid proposal ID");
-        Proposal storage proposal = proposals[proposalId];
-        require(!proposal.executed, "Proposal already executed");
-        require(proposal.affirmativeVotes > proposal.negativeVotes, "More votes against than for");
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        uint256 totalVotes = proposal.affirmativeVotes + proposal.negativeVotes;
-        require(totalVotes >= (totalSupply() * minQuorumPercent) / 100, "Quorum not met");
-
-        proposal.executed = true;
-        emit ProposalExecuted(proposalId, true, proposal.description);
-        // Implementation logic based on the proposal's description or id
-    }
-
     function adjustTransactionFee(uint256 newFeeRate) public onlyRole(ADMIN_ROLE) {
         require(newFeeRate <= 100, "Fee rate too high"); // Max 1%
         transactionFeeRate = newFeeRate;
+        emit TransactionFeeAdjusted(newFeeRate);
     }
+
+    function updateTreasuryAddress(address newAddress) public onlyRole(ADMIN_ROLE) {
+        require(newAddress != address(0), "Invalid address");
+        treasuryAddress = newAddress;
+        emit TreasuryAddressUpdated(newAddress);
+    }
+
+    function grantRole(bytes32 role, address account) public override onlyRole(getRoleAdmin(role)) {
+        super.grantRole(role, account);
+        emit RoleGranted(role, account);
+    }
+
+    function revokeRole(bytes32 role, address account) public override onlyRole(getRoleAdmin(role)) {
+        super.revokeRole(role, account);
+        emit RoleRevoked(role, account);
+    }
+
+    // Additional functions and modifiers can be implemented as needed
 }
